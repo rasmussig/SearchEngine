1861f0fc5b6055ccf1b21778c08173839256e643
Rasmussig <rasmus2001@live.dk>
2025-09-03 19:23:52 +0200

    Data_Path_Compatibility

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+
 namespace Shared
 {
     public class Paths
     {
-    public static string DATABASE = @"C:\Users\rasmu\OneDrive\Skrivebord\EAA - Noter\6Semester\Arkitekturprincipper i praksis\SearchEngineProj\Data\searchDBmedium.db";
+        // Find the project root (where SearchEngine.sln is located)
+        private static string GetProjectRoot()
+        {
+            string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            
+            // Go up directories until we find SearchEngine.sln
+            while (currentDir != null && !File.Exists(Path.Combine(currentDir, "SearchEngine.sln")))
+            {
+                currentDir = Directory.GetParent(currentDir)?.FullName;
+            }
+            
+            if (currentDir == null)
+            {
+                throw new DirectoryNotFoundException("Could not find project root containing SearchEngine.sln");
+            }
+            
+            return currentDir;
+        }
+        
+        // Get the Data folder path relative to project
+        public static string GetDataPath()
+        {
+            string projectRoot = GetProjectRoot();
+            // Go up one level from SearchEngine folder to get to the main folder, then into Data
+            string dataPath = Path.Combine(Directory.GetParent(projectRoot).FullName, "Data");
+            
+            if (!Directory.Exists(dataPath))
+            {
+                throw new DirectoryNotFoundException($"Data folder not found at: {dataPath}");
+            }
+            
+            return dataPath;
+        }
+        
+        public static string DATABASE = Path.Combine(GetDataPath(), "searchDBmedium.db");
     }
 }
--- indexer/Config.cs
+++ indexer/Config.cs
@@ -1,30 +1,67 @@
 
 using System;
+using System.IO;
+using System.Reflection;
+
 namespace Indexer;
 
 public class Config
 {
+    // Find the project root (where SearchEngine.sln is located)
+    private static string GetProjectRoot()
+    {
+        string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        
+        // Go up directories until we find SearchEngine.sln
+        while (currentDir != null && !File.Exists(Path.Combine(currentDir, "SearchEngine.sln")))
+        {
+            currentDir = Directory.GetParent(currentDir)?.FullName;
+        }
+        
+        if (currentDir == null)
+        {
+            throw new DirectoryNotFoundException("Could not find project root containing SearchEngine.sln");
+        }
+        
+        return currentDir;
+    }
+    
+    // Get the Data folder path relative to project
+    private static string GetDataPath()
+    {
+        string projectRoot = GetProjectRoot();
+        // Go up one level from SearchEngine folder to get to the main folder, then into Data
+        string dataPath = Path.Combine(Directory.GetParent(projectRoot).FullName, "Data");
+        
+        if (!Directory.Exists(dataPath))
+        {
+            throw new DirectoryNotFoundException($"Data folder not found at: {dataPath}");
+        }
+        
+        return dataPath;
+    }
+    
     // the folder to be indexed - all .txt files in that folder (and subfolders)
     // will be indexed
-    public static string BASE_PATH = @"C:\Users\rasmu\OneDrive\Skrivebord\EAA - Noter\6Semester\Arkitekturprincipper i praksis\SearchEngineProj\Data\seData copy";
-    public static string FOLDER = BASE_PATH + "\\medium";
+    public static string BASE_PATH = GetDataPath();
+    public static string FOLDER = Path.Combine(BASE_PATH, "medium");
 
     public static void SelectFolder(string size)
     {
         switch (size.ToLower())
         {
             case "large":
-                FOLDER = BASE_PATH + "\\large";
+                FOLDER = Path.Combine(BASE_PATH, "large");
                 break;
             case "medium":
-                FOLDER = BASE_PATH + "\\medium";
+                FOLDER = Path.Combine(BASE_PATH, "medium");
                 break;
             case "small":
-                FOLDER = BASE_PATH + "\\small";
+                FOLDER = Path.Combine(BASE_PATH, "small");
                 break;
             default:
                 Console.WriteLine("Ukendt valg, bruger medium.");
-                FOLDER = BASE_PATH + "\\medium";
+                FOLDER = Path.Combine(BASE_PATH, "medium");
                 break;
         }
     }
